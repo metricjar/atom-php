cb483e320736c6e1f1f8bb2598a70cec754ad32d
Kirill Bokhanov <kirill.bokhanov@ironsrc.com>
2016-07-25 16:22:19 +0300

    unit test support added to the project

---
+++ test/AtomTest.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: kirill.bokhanov
+ * Date: 7/25/16
+ * Time: 1:36 PM
+ */
+
+
+namespace IronSourceAtom;
+use PHPUnit\Framework\TestCase;
+
+class AtomTest extends TestCase{
+    public function testAtomConstruct(){
+        $this->assertTrue(true);
+    }
+}
