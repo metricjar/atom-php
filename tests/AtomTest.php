8385715eb1403c61e2efaeb94f7fa49d3dc883cb
Kirill Bokhanov <kirill.bokhanov@ironsrc.com>
2016-07-27 15:36:07 +0300

    code coverage activated

--- tests/AtomTest.php
+++ tests/AtomTest.php
@@ -3,19 +3,18 @@
  */
 
 
+namespace IronSourceAtom;
 
-include '../lib/Atom.php';
 
 class AtomTest extends \PHPUnit_Framework_TestCase
 {
 
     /**
-     * @covers Atom::__construct
      * @expectedException InvalidArgumentException
      */
     public function testAtomConstructNullAuth()
     {
-        new IronSourceAtom\Atom(null);
+        new Atom(null);
     }
 
 
