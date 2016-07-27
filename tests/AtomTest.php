7868c76b623eef2673084faf23191235efe5b691
Kirill Bokhanov <kirill.bokhanov@ironsrc.com>
2016-07-27 15:58:33 +0300

    new tests added

--- tests/AtomTest.php
+++ tests/AtomTest.php
@@ -17,5 +17,23 @@ class AtomTest extends \PHPUnit_Framework_TestCase
         new Atom(null);
     }
 
+    /**
+     * @expectedException InvalidArgumentException
+     */
+    public function testPutEventNullStream()
+    {
+        $atom = new Atom('');
+        $atom->putEvent(null, 'fff');
+    }
+
+    /**
+     * @expectedException InvalidArgumentException
+     */
+    public function testPutEventEmptyStream()
+    {
+        $atom = new Atom('');
+        $atom->putEvent('', 'fff');
+    }
+
 
 }
